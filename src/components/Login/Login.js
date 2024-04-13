c3e1aadc6999da93bcd8b90687dd1c9249aef32d
Zaid Alam <zaidalam0731@gmail.com>
2024-04-13 17:03:39 -0400

    add /api from login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -37,7 +37,7 @@ function Login() {
     
         try {
          
-          const response = await axios.post("/login", userCredentials);
+          const response = await axios.post("/api/login", userCredentials);
           if (response.data.message === "Incorrect password") {
               alert("Invalid Credentials");
           } else if (response.data.message === "User not found") {
